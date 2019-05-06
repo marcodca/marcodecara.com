210102c907c5429d3edcdf057feba5629181f110
marco_dca <marcodecara@gmail.com>
2019-05-06 10:28:20 +0200

    credits added for the background svg

--- src/styles/global.jsx
+++ src/styles/global.jsx
@@ -17,6 +17,7 @@ const GlobalStyle = createGlobalStyle`
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
+  /* background by SVGBackgrounds.com */
   html, body {
     width: 100%;
     height: 100%;
