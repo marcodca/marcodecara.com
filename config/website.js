872037778c88d53b67dbe8772a4532cb3126fe21
marco_dca <marcodecara@gmail.com>
2019-05-06 10:37:28 +0200

    website.js info customized

--- config/website.js
+++ config/website.js
@@ -3,19 +3,19 @@ const tailwind = require('../tailwind')
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
-  siteTitle: 'Cara - Gatsby Starter Portfolio', // Navigation and Site Title
-  siteTitleAlt: 'Cara', // Alternative Site title for SEO
-  siteTitleShort: 'Cara', // short_name for manifest
-  siteHeadline: 'Creating marvelous art & blazginly fast websites', // Headline for schema.org JSONLD
-  siteUrl: 'https://cara.lekoarts.de', // Domain of your site. No trailing slash!
+  siteTitle: 'Marco de Cara', // Navigation and Site Title
+  siteTitleAlt: 'Marco, web developer in Copenhagen', // Alternative Site title for SEO
+  siteTitleShort: 'MarcoDC', // short_name for manifest
+  siteHeadline: 'Marco de Cara is a web developer based in Copenhagen, he likes React, Gatsby, styled-components, and a bunch of other stuff', // Headline for schema.org JSONLD
+  siteUrl: 'https://marcodecara.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logo.png', // Used for SEO and manifest
-  siteDescription: 'Playful & Colorful One-Page website with Parallax effect',
-  author: 'LekoArts', // Author for schema.org JSONLD
+  siteDescription: "Marco de Cara's personal website. He is a web developer, by the way",
+  author: 'Marco de Cara', // Author for schema.org JSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  //userTwitter: '@cara', // Twitter Username
-  //ogSiteName: 'cara', // Facebook Site Name
+  //userTwitter: '', // Twitter Username
+  //ogSiteName: '', // Facebook Site Name
   //ogLanguage: 'en_US', // Facebook Language
   googleAnalyticsID: 'UA-139454760-1',
 
