05d711eec3b8a054034efa62cd3d496b2870f7f9
marco_dca <marcodecara@gmail.com>
2019-05-13 00:33:51 +0200

    advances in the view

--- src/views/Projects.jsx
+++ src/views/Projects.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import tw from 'tailwind.macro'
 import { Divider, DividerMiddle } from '../elements/Dividers'
 import Content from '../elements/Content'
 import Inner from '../elements/Inner'
+import WebTechs from '../components/WebTechs'
 import { UpDown, UpDownWide } from '../styles/animations'
 import { colors } from '../../tailwind'
 
+
+const WebTechsWrapper = styled.div`
+  ${tw`absolute w-5/6 flex items-center justify-center h-auto`};
+  top: 60%;
+`;
 
 const Projects = ({ children, offset }) => (
   <>
@@ -17,6 +25,9 @@ const Projects = ({ children, offset }) => (
     />
     <Content speed={0.4} offset={`${offset}.2`} factor={2}>
       <Inner>{children}</Inner>
+      <WebTechsWrapper>
+        <WebTechs/>
+      </WebTechsWrapper>
     </Content>
     <Divider speed={0.1} offset={offset} factor={2}>
       <UpDown>
