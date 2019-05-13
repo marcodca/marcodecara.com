ecda3dff741dbabab8b89f98175251edde1dd026
marco_dca <marcodecara@gmail.com>
2019-05-13 10:13:58 +0200

    advancing on webTechs

--- src/components/WebTechs.jsx
+++ src/components/WebTechs.jsx
@@ -1,18 +1,19 @@
-import React from "react";
-import styled, { css } from "styled-components";
-import { useSpring, animated } from "react-spring";
-import tw from "tailwind.macro";
+import React from "react"
+import styled from "styled-components"
+import { useSpring, animated } from "react-spring"
+import tw from "tailwind.macro"
 //icons
-import reactIcon from "../images/icons/react-icon.svg";
-import gatbsyIcon from "../images/icons/gatsby-icon.svg";
-import styledComponentsIcon from "../images/icons/styled-components-icon.svg";
+import reactIcon from "../images/icons/react-icon.svg"
+import gatbsyIcon from "../images/icons/gatsby-icon.svg"
+import styledComponentsIcon from "../images/icons/styled-components-icon.svg"
+import graphQlIcon from "../images/icons/graphql-icon.svg"
 
 
 const calc = (x, y) => [x - window.innerWidth / 2, y - window.innerHeight / 2];
 const trans1 = (x, y) => `translate3d(${x / 10}px,${y / 10}px,0)`;
 const trans2 = (x, y) => `translate3d(${x / 8 + 35}px,${y / 8 - 70}px,0)`;
-const trans3 = (x, y) => `translate3d(${x / 6 - 250}px,${y + 200}px,0)`;
-const trans4 = (x, y) => `translate3d(${x / 3.5}px,${y / 3.5}px,0)`;
+const trans3 = (x, y) => `translate3d(${x / 6 - 120}px,${y + 200}px,0)`;
+const trans4 = (x, y) => `translate3d(${x / 3.5 + 130}px,${y / 3.5 + 210}px,0)`;
 
 export const Container = styled.div`
   ${tw`w-1/3 flex align-center justify-center h-auto`}
@@ -25,8 +26,8 @@ const Icon = styled(animated.div)`
   background-position: center center;
   background-repeat: no-repeat;
   will-change: transform;
-  min-width: ${props => props.size * 7}px;
-  min-height: ${props => props.size * 7}px;
+  min-width: ${props => props.size * 12}px;
+  min-height: ${props => props.size * 12}px;
   width: ${props => props.size}vw;
   height: ${props => props.size}vw;
   max-width: ${props => props.size * 1.5 }vw;
@@ -58,8 +59,12 @@ const WebTechs = () => {
         size={11}
         icon={styledComponentsIcon} 
         style={{ transform: props.xy.interpolate(trans3) }}
-        />
-      {/* <GatsbyIcon style={{ transform: props.xy.interpolate(trans2) }}/> */}
+      />
+      <Icon
+        size={11}
+        icon={graphQlIcon} 
+        style={{ transform: props.xy.interpolate(trans4) }}
+      />
     </Container>
   );
 };
