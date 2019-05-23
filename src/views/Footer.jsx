ccc31a9c70c543c7cf5c72c913a4743818009073
marco_dca <marcodecara@gmail.com>
2019-05-23 10:54:48 +0200

    done with credits

--- src/views/Footer.jsx
+++ src/views/Footer.jsx
@@ -4,7 +4,7 @@ import tw from "tailwind.macro";
 import githubIcon from "../images/icons/github-icon.svg";
 import logo from "../images/logo.png";
 import Modal from "react-responsive-modal";
-import spot from '../images/spot.svg'
+import spot from "../images/spot.svg";
 
 const StyledFooter = styled.footer`
   ${tw`text-center w-full md:w-2/3 lg:w-1/3 flex justify-around items-center text-grey absolute pin-b p-6 font-sans text-md lg:text-lg`};
@@ -13,21 +13,20 @@ const StyledFooter = styled.footer`
 const Ul = styled.ul`
   ${tw`leading-relaxed font-sans p-4`}
   list-style : none;
-`
-
-const Li = styled.li`
-  ${tw`relative`}
-  ::before {
-    content : "";
-    width: 12px;
-    height: 12px;
-    background: url(${spot});
-    background-size: 12px;
-    position: absolute;
-    left: -15px;
-    top: 7px;
+  li {
+    ${tw`relative`}
+    ::before {
+      content: "";
+      width: 12px;
+      height: 12px;
+      background: url(${spot});
+      background-size: 12px;
+      position: absolute;
+      left: -15px;
+      top: 7px;
+    }
   }
-`
+`;
 
 const Footer = () => {
   const [open, setOpen] = useState(false);
@@ -60,45 +59,52 @@ const Footer = () => {
       </a>
 
       <Modal open={open} onClose={() => setOpen(false)} center>
+        <p
+          css={`${tw`font-sans pt-4 -mb-4 text-lg font-semibold`}`}
+        >
+          Project made in <a href="https://www.reactjs.org/">React</a> and
+          <a href="https://www.gatsbyjs.org/"> GatsbyJs</a>
+        </p>
         <Ul>
           <li>
-            Project made in <a href="https://www.reactjs.org/">React</a> and
-            <a href="https://www.gatsbyjs.org/"> GatsbyJs</a>
-          </li>
-          <Li>
-            Gatsby starter  
-            <a href="https://github.com/LekoArts/gatsby-starter-portfolio-cara"> Cara </a>
+            Gatsby starter
+            <a href="https://github.com/LekoArts/gatsby-starter-portfolio-cara">
+              {" "}
+              Cara{" "}
+            </a>
             by <a href="https://www.lekoarts.de/"> Lekoarts</a>
-          </Li>
-          <Li>
+          </li>
+          <li>
             <a href="https://github.com/KyleAMathews/typefaces">Typefaces</a> by
             <a href="https://www.bricolage.io/"> Kyle Mathews</a>
-          </Li>
-          <Li>
+          </li>
+          <li>
             <a href="https://www.styled-components.com/">styled-components</a>
-          </Li>
-          <Li>
+          </li>
+          <li>
             <a href="https://tailwindcss.com/"> Tailwind css</a>
-          </Li>
-          <Li>
+          </li>
+          <li>
             <a href="https://www.react-spring.io/"> React spring</a>
-          </Li>
-          <Li>
+          </li>
+          <li>
             Background by
             <a href="https://www.svgbackgrounds.com/"> SVG backgrounds</a>
-          </Li>
-          <Li>
+          </li>
+          <li>
             Icons by <a href="https://www.flaticon.com/"> flat icon</a>
-          </Li>
-          <Li>
+          </li>
+          <li>
             <a href="https://www.npmjs.com/package/react-blob">react-blob</a>
-          </Li>
-          <Li>
-            <a href="https://www.npmjs.com/package/react-responsive-modal">react-responsive-modal</a>
-          </Li>
-          <Li>
+          </li>
+          <li>
+            <a href="https://www.npmjs.com/package/react-responsive-modal">
+              react-responsive-modal
+            </a>
+          </li>
+          <li>
             <a href="https://realfavicongenerator.net/">RealFaviconGenerator</a>
-          </Li>
+          </li>
         </Ul>
       </Modal>
     </StyledFooter>
