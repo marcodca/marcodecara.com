4710f5b5335081cec911e631c6d7741220bcae1a
marco_dca <marcodecara@gmail.com>
2020-01-26 00:23:35 +0100

    advancing on the project card

--- src/elements/ProjectCard.jsx
+++ src/elements/ProjectCard.jsx
@@ -52,16 +52,23 @@ const ProjectCard = ({ projectData, isExpanded, setExpandedCard, i }) => {
     }
   `);
 
+
+
   const { title, description } = projectData;
 
   const ProjectBox = styled(animated.div)`
     ${tw`w-4/5 md:w-3/5 lg:w-2/5 lg:h-64 m-auto  border-4 border-solid border-grey-darker rounded-lg`}
     /* width: 30%; */
   /* min-width: 200px; */
-  min-width: 200px ;
-  min-height: 300px;
+  min-width: 280px;
+    min-height: 300px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    overflow-y: scroll;
 
-  /* position: absolute; */
+    /* position: absolute; */
     background: white;
   `;
   const ProjectImage = styled(Img)`
@@ -85,28 +92,30 @@ const ProjectCard = ({ projectData, isExpanded, setExpandedCard, i }) => {
     zIndex: -1,
     position: "relative",
     left: "0%",
-    height: "10vh",  
-    width: '30vw',
-    transform: "rotate(0deg) translateX(0%)",
+    height: "10vh",
+    width: "30vw",
+    transform: "rotate(0deg) translateX(0%) rotateY(0deg)",
     boxShadow: "72px 69px 33px -19px rgba(0,0,0,0.63)",
+    minWidth: "0px"
   };
   const projectBoxSelectedProps = {
     zIndex: 0,
-    position: "absolute",
     left: "50%",
-    height: "100vh",  
-    width: "80vw",
-    transform: "rotate(-15deg) translateX(-50%)",
+    position: "absolute",
+    height: "100vh",
+    width: "110vw",
+    transform: "rotate(-15deg) translateX(-50%) rotateY(50deg)",
     boxShadow: "0px 0px 33px 40px rgba(0,0,0,0.3)",
-  }
+    minWidth: "730px"
+  };
 
   const projectBoxAnimation = useSpring({
     from: projectBoxNotSelectedProps,
-    to: isExpanded
-    ? projectBoxSelectedProps
-    : projectBoxNotSelectedProps,
-    config: config.slow
+    to: isExpanded ? projectBoxSelectedProps : projectBoxNotSelectedProps
+    // config: config.slow
   });
+
+  const sampleContent =  "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad corporis deleniti facere quisquam quae adipisci perferendis reiciendis nulla voluptates. Officia quae pariatur voluptate quod vero, quis amet tempore rerum enim suscipit voluptates ipsa minus vel quia ratione sunt corporis. Atque, necessitatibus soluta! Mollitia earum voluptatibus amet quam quis omnis magni doloribus, vero labore rerum ipsa aliquam facilis inventore placeat quibusdam maiores nulla sunt est numquam consequatur cupiditate natus eum perspiciatis. Dolor tenetur recusandae praesentium itaque, esse necessitatibus corrupti, adipisci voluptas veniam sint natus odio aliquid labore consequuntur consectetur ullam possimus! Voluptatem porro laborum culpa dolor temporibus fugiat voluptates blanditiis magnam omnis, dolorem, eius rem, aut perspiciatis corporis nostrum. Quod quaerat commodi aliquam nulla dolore delectus? Eum iste eaque maxime quasi Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad corporis deleniti facere quisquam quae adipisci perferendis reiciendis nulla voluptates. Officia quae pariatur voluptate quod vero, quis amet tempore rerum enim suscipit voluptates ipsa minus vel quia ratione sunt corporis. Atque, necessitatibus soluta! Mollitia earum voluptatibus amet quam quis omnis magni doloribus, vero labore rerum ipsa aliquam facilis inventore placeat quibusdam maiores nulla sunt est numquam consequatur cupiditate natus eum perspiciatis. Dolor tenetur recusandae praesentium itaque, esse necessitatibus corrupti, adipisci voluptas veniam sint natus odio aliquid labore consequuntur consectetur ullam possimus! Voluptatem porro laborum culpa dolor temporibus fugiat voluptates blanditiis magnam omnis, dolorem, eius rem, aut perspiciatis corporis nostrum. Quod quaerat commodi aliquam nulla dolore delectus? Eum iste eaque maxime quasi"
 
   const shadeAnimation = useSpring({
     from: {
@@ -119,6 +128,18 @@ const ProjectCard = ({ projectData, isExpanded, setExpandedCard, i }) => {
       duration: 1200
     }
   });
+
+  const ImgWrapperOpenAnimation = { width: "50%", height: "50%"  };
+  const ImgWrapperClosedAnimation = { width: "100%",  height: "100%"};
+
+  const ImgWrapperAnimation = useSpring({
+    from: ImgWrapperClosedAnimation,
+    to: isExpanded ? ImgWrapperOpenAnimation : ImgWrapperClosedAnimation
+  });
+
+  const ImgWrapper = styled(animated.div)`
+    /* overflow: hidden; */
+  `
 
   const innerContentAnimation = useSpring({
     from: { marginLeft: "0px" },
@@ -143,12 +164,14 @@ const ProjectCard = ({ projectData, isExpanded, setExpandedCard, i }) => {
       }}
     >
       <ProjectTitle>{title}</ProjectTitle>
-      <ProjectImage
-        objectFit="cover"
-        objectPosition="left bottom"
-        alt=""
-        fluid={data.placeholderImage.childImageSharp.fluid}
-      ></ProjectImage>
+      <ImgWrapper style={ImgWrapperAnimation}>
+        <ProjectImage
+          objectFit="cover"
+          objectPosition="right bottom"
+          alt=""
+          fluid={data.placeholderImage.childImageSharp.fluid}
+        />
+      </ImgWrapper>
       {/* {isExpanded && <Shade style={shadeAnimation} />} */}
       {/* {innerTransition.map(
         ({ item, key, props }) => item && <Shade key={key} style={props} />
@@ -171,6 +194,7 @@ const ProjectCard = ({ projectData, isExpanded, setExpandedCard, i }) => {
         )}
       </Content>
       <Border /> */}
+      {isExpanded && sampleContent }
     </ProjectBox>
   );
 };
