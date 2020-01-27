9147dbc61ae0061292be432cf637cdcd5086a82e
marco_dca <marcodecara@gmail.com>
2020-01-27 15:58:44 +0100

    Done with basic styling

--- src/elements/ProjectCard.jsx
+++ src/elements/ProjectCard.jsx
@@ -6,46 +6,20 @@ import { animated, useSpring, useTransition, config } from "react-spring";
 import { media } from "../styles/utils";
 import { colors } from "../../tailwind";
 import tw from "tailwind.macro";
-import reffenStock from "../images/screens/reffen-stock.png";
-
-const Shade = styled(animated.div)`
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  /* top: 0; */
-  left: 0;
-  bottom: 10px;
-  background: linear-gradient(
-    180deg,
-    rgba(255, 255, 255, 0) 0%,
-    rgba(255, 255, 255, 1) 75%,
-    rgba(255, 255, 255, 1) 100%,
-    rgba(255, 255, 255, 1) 100%
-  );
-  /* background: red; */
-  pointer-events: none;
-`;
-
-const Content = styled.div`
-  padding: 5px;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-  z-index: 2;
-`;
-
-const Border = styled.div`
-  background: ${colors.black};
-`;
+import crossIcon from "../images/icons/cross-icon.svg";
+import siteIcon from "../images/icons/site-icon.svg";
+import githubIcon from "../images/icons/github-icon.svg";
 
 const ProjectCard = ({ projectData, isExpanded, setExpandedCard, i }) => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "screens/reffen-stock.png" }) {
-        childImageSharp {
-          fluid(maxWidth: 1200) {
+      placeholderImage: allImageSharp {
+        edges {
+          node {
+            fluid(maxWidth: 800) {
+            originalName
             ...GatsbyImageSharpFluid
+            }
           }
         }
       }
@@ -58,25 +32,42 @@ const ProjectCard = ({ projectData, isExpanded, setExpandedCard, i }) => {
     ${tw`w-4/5 md:w-3/5 lg:w-2/5 lg:h-64 m-auto  border-4 border-solid border-grey-darker rounded-lg`}
     /* width: 30%; */
   /* min-width: 200px; */
+    background: linear-gradient( white, grey);
     min-width: 280px;
     min-height: 300px;
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: center;
-    background: white;
+    justify-content: flex-start;
+    /* background: white; */
     grid-column: ${i + 1 === 1 ? "1" : "2"} / span 1;
     grid-row: ${i + 1 === 3 ? "2" : "1"} / span 1;
-    ${media.md` grid-row: ${i +
+    ${media.md` height: 50vh ; grid-row: ${i +
       1} / span 1; grid-column: 1 / span 1; transform: translateX(15%);`}
   `;
-  const ProjectImage = styled(Img)`
-    ${tw`h-full w-full`};
+
+  const TopContainer = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin-top: 7.5%;
+    ${media.sm`margin-top: ${props => props.isExpanded && `10%`}`};
   `;
 
-  const ProjectTitle = styled.h3`
-    ${tw`m-0 p-3 w-full absolute z-10 border-radius-10 font-serif`} bottom: 0;
+  const ProjectImage = styled(Img)`
+    ${tw`h-full w-full`};
+    border-radius: 5px;
+    border: "10px solid rgba(0,0,0, 0.6)";
+  `;
+
+  const BottomContent = styled.div`
+    ${tw`m-0 w-full absolute z-10 border-radius-10 font-serif`} bottom: 0;
     background: ${colors["grey-light"]};
+    padding: ${props => props.isExpanded && `1vh`};
+    font-size: ${props => !props.isExpanded && `24px`};
+    ${media.sm`font-size: ${props => props.isExpanded && `15px`};`}
   `;
   const projectBoxNotSelectedProps = {
     zIndex: "-1",
@@ -112,18 +103,40 @@ const ProjectCard = ({ projectData, isExpanded, setExpandedCard, i }) => {
   const sampleContent =
     "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad corporis deleniti facere quisquam quae adipisci perferendis reiciendis nulla voluptates. Officia quae pariatur voluptate quod vero, quis amet tempore rerum enim suscipit voluptates ipsa minus vel quia ratione sunt corporis. Atque, necessitatibus soluta! Mollitia earum voluptatibus amet quam";
 
-  const ImgWrapperOpenAnimation = {
-    width: "50%",
-    minWidth: "200px",
-    border: "2px solid rgba(0,0,0, 0.6)",
-    maxHeight: "400px"
-  };
-  const ImgWrapperClosedAnimation = {
-    width: "100%",
-    minWidth: "0px",
-    border: "0px solid rgba(0,0,0, 0.6)",
-    maxHeight: "1000px"
-  };
+
+
+  const ActionButtons = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+    font-family: Questrial;
+    font-size: 1em;
+    width: 75%;
+    min-width: 280px;
+    div {
+      display: flex;
+      align-items: center;
+      padding: 5px;
+    }
+    img {
+      width: 1rem;
+      margin: 3px;
+    }
+    .github {
+      filter: invert(100%);
+    }
+  `;
+
+const ImgWrapperOpenAnimation = {
+  width: "50%",
+  minWidth: "200px",
+  maxHeight: "400px",
+  top: "unset"
+};
+const ImgWrapperClosedAnimation = {
+  width: "100%",
+  minWidth: "0px",
+  maxHeight: "1000px",
+};
 
   const ImgWrapperAnimation = useSpring({
     from: ImgWrapperClosedAnimation,
@@ -136,10 +149,17 @@ const ProjectCard = ({ projectData, isExpanded, setExpandedCard, i }) => {
 
   const CloseCardButton = styled.span`
     position: absolute;
-    right: 0;
-    top: 0;
-    font-size: 24px;
+    right: 15px;
+    top: 15px;
+    width: 3%;
+    cursor: pointer;
+    min-width: 20px;
+    img {
+      width: 100%;
+    }
   `;
+
+  console.log(data);
 
   return (
     <ProjectBox
@@ -152,24 +172,41 @@ const ProjectCard = ({ projectData, isExpanded, setExpandedCard, i }) => {
             }
       }
     >
-      <ProjectTitle>{title}</ProjectTitle>
-      <ImgWrapper style={ImgWrapperAnimation}>
-        <ProjectImage
-          alt={`${title} screen`}
-          fluid={data.placeholderImage.childImageSharp.fluid}
-          imgStyle={{ objectFit: "contain" }}
-        />
-      </ImgWrapper>
+      <TopContainer isExpanded={isExpanded}>
+        <ImgWrapper style={ImgWrapperAnimation}>
+          <ProjectImage
+            alt={`${title} screen`}
+            fluid={data.placeholderImage.edges[3].node.fluid}
+            imgStyle={{ objectFit: "contain" }}
+          />
+        </ImgWrapper>
+        {isExpanded && (
+          <ActionButtons>
+            <div>
+              <img src={siteIcon} />
+              Visit site
+            </div>
+            <div>
+              <img src={githubIcon} className="github" />
+              View source
+            </div>
+          </ActionButtons>
+        )}
+      </TopContainer>
+
       {isExpanded && (
         <CloseCardButton
           onClick={() => {
             setExpandedCard(0);
           }}
         >
-          CLOSE
+          <img src={crossIcon} width={26} />
         </CloseCardButton>
       )}
-      {isExpanded && sampleContent}
+      <BottomContent isExpanded={isExpanded}>
+        <h3>{title}</h3>
+        {isExpanded && sampleContent}
+      </BottomContent>
     </ProjectBox>
   );
 };
