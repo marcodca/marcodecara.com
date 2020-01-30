926377f2002ed031848e870bae89a4171893f83f
marco_dca <marcodecara@gmail.com>
2020-01-30 12:12:44 +0100

    Test parallax-js on deploy

--- src/components/WebTechs.jsx
+++ src/components/WebTechs.jsx
@@ -1,25 +1,18 @@
-import React from "react"
+import React, {useRef, useEffect}  from "react"
 import styled from "styled-components"
-import { useSpring, animated } from "react-spring"
 import tw from "tailwind.macro"
+import Parallax from 'parallax-js'
 //icons
 import reactIcon from "../images/icons/react-icon.svg"
 import gatbsyIcon from "../images/icons/gatsby-icon.svg"
 import styledComponentsIcon from "../images/icons/styled-components-icon.svg"
 import graphQlIcon from "../images/icons/graphql-icon.svg"
 
-
-const calc = (x, y) => [x - window.innerWidth / 2, y - window.innerHeight / 2];
-const trans1 = (x, y) => `translate3d(${x / 10}px,${y / 10}px,0)`;
-const trans2 = (x, y) => `translate3d(${x / 8 + 35}px,${y / 8 - 70}px,0)`;
-const trans3 = (x, y) => `translate3d(${x / 6 - 120}px,${y + 200}px,0)`;
-const trans4 = (x, y) => `translate3d(${x / 3.5 + 130}px,${y / 3.5 + 210}px,0)`;
-
-const Container = styled.div`
+const Scene = styled.div`
   ${tw`w-1/3 flex align-center justify-center h-1/3`}
 `;
 
-const Icon = styled(animated.div)`
+const Icon = styled.div`
   position: absolute;
   border-radius: 5px;
   background-size: cover;
@@ -36,36 +29,32 @@ const Icon = styled(animated.div)`
 `;
 
 const WebTechs = () => {
-  const [props, set] = useSpring(() => ({
-    xy: [0, 0],
-    config: { mass: 10, tension: 550, friction: 140 }
-  }));
+
+  const sceneRef = useRef();
+
+  useEffect(()=>{
+    console.log(sceneRef.current);
+
+    //eslint-disable-next-line
+    const parallax = new Parallax(sceneRef.current);
+  },[])
 
   return (
-    <Container
-      onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}
+    <Scene
+    ref={sceneRef}
     >
       <Icon
-        size={35}
-        icon={reactIcon} 
-        style={{ transform: props.xy.interpolate(trans1) }}
-        />
-      <Icon
-        size={13}
-        icon={gatbsyIcon} 
-        style={{ transform: props.xy.interpolate(trans2) }}
-        />
-      <Icon
-        size={11}
-        icon={styledComponentsIcon} 
-        style={{ transform: props.xy.interpolate(trans3) }}
+        size={16}
+        data-depth="0.60"
+        icon={reactIcon}
       />
       <Icon
-        size={11}
-        icon={graphQlIcon} 
-        style={{ transform: props.xy.interpolate(trans4) }}
+        size={24}
+        data-depth="0.80"
+        icon={graphQlIcon}
       />
-    </Container>
+
+    </Scene>
   );
 };
 
